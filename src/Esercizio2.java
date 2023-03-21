45ced910b8c21a5decd2cda94e7a2f07f5053f72
vincenzopicone <vincenzopicone88@icloud.com>
2023-03-21 16:37:39 +0100

    final

---
+++ src/Esercizio2.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class Esercizio2 {
+    public static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        List<Integer> listaNumeri = new ArrayList<Integer>();
+
+        Random random = new Random();
+        System.out.println("Quanti numeri vuoi generare?");
+        int quantità = scanner.nextInt();
+        for (int i = 0; i < quantità; i++) {
+            int number = random.nextInt(100);
+            listaNumeri.add(number);
+        }
+        System.out.println(quantità + " Numeri casuali: " + listaNumeri.toString());
+        Collections.sort(listaNumeri);
+        System.out.println(quantità + " Numeri ordinati: " + listaNumeri.toString());
+        ordineInverso(listaNumeri);
+    }
+    public static void ordineInverso(List lista) {
+        Collections.sort(lista, Collections.reverseOrder());
+        System.out.println("Lista numeri ordine inverso: " + lista.toString());
+        ordinePariDispari(lista, false );
+    }
+    public static void ordinePariDispari(List list, boolean b) {
+        if (b = true) {
+            System.out.println("Posizione pari");
+            for (int i = 0; i < list.size(); i++) {
+                if (i % 2 == 0) {
+                    System.out.print(list.get(i) + ",");
+                }
+            }
+        } else {
+            System.out.println("Posizione dispari");
+            for (int i = 0; i < list.size(); i++) {
+                if (i % 2 != 0) {
+                    System.out.print(list.get(i) + ",");
+                }
+            }
+        }
+    }
+}
---
+++ src/Esercizio3.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+public class Esercizio3 {
+    public static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+
+        Map<String, Integer> rubrica = new HashMap<String, Integer>();
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Inserisci il nome");
+            String nome = scanner.next();
+            System.out.println("Inserisci il numero");
+            int numero = scanner.nextInt();
+            rubrica.put(nome, numero);
+        }
+        System.out.println(rubrica.toString());
+        System.out.println("Quale contatto vuoi cancellare?");
+        String nomeDaCancellare = scanner.next();
+        rubrica.remove(nomeDaCancellare);
+        System.out.println(rubrica.toString());
+        System.out.println("Ricerca per nome!");
+        String nomeDaRicercare = scanner.next();
+        ricercaDaNome((Map) rubrica, nomeDaRicercare);
+        System.out.println("");
+        System.out.println("Ricerca per numero!");
+        int numeroDaRicercare = scanner.nextInt();
+        ricercaDaNumero((Map) rubrica, numeroDaRicercare);
+    }
+
+
+
+    public static void ricercaDaNome(Map list, String x) {
+        System.out.print(list.get(x));
+    }
+    public static void ricercaDaNumero(Map list, int x) {
+       System.out.println(getKey(list, x));
+    }
+    public static <K, V> K getKey(Map<K, V> map, V value)
+    {
+        for (Map.Entry<K, V> entry: map.entrySet())
+        {
+            if (value.equals(entry.getValue())) {
+                return entry.getKey();
+            }
+        }
+        return null;
+    }
+}
